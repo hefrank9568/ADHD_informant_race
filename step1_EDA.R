4fbc69065f20d448b7c63942bb2271a0914e8962
Penny Peng <93160894+Capybara-Pen@users.noreply.github.com>
2022-12-14 21:04:08 -0600

    Merge file (attempt 1)
    all KSADS_14_ are missing

--- step1_EDA.R
+++ step1_EDA.R
@@ -3,9 +3,10 @@
 library(here)
 library(dplyr)
 library(data.table)
+library(tidyverse)
 rm(list = ls())
 #####set path to data#####
-path_dat <- "/Users/quanfahe/OneDrive - UW-Madison/GradSchool/Research/ABCD/"##change this for your environment
+path_dat <- "/Users/zpeng/OneDrive/Desktop/ABCD/Analysis data/ADHD_informant_race/"##change this for your environment
 here()
 ####Load separate data and select relevant variables####
 ####race-ethnicity load from ABCD-invariance project####
@@ -21,10 +22,12 @@ abcd_cbcldemo <- abcd_cbcldemo %>%
 abcd_cbcldemo <- abcd_cbcldemo %>%
   filter(Race_three %in% c(0,1,2))
 table(abcd_cbcldemo$Race_three)
+
 ##remove all variables except for race-ethnicity
 abcd_race <- abcd_cbcldemo %>%
   select(Race_three, subjectkey,hispanic)
 rm(list = c("abcd_cbcldemo","abcd_anthro","abcd_medServ"))###these are not relevant for this study but loaded together with cbcldemo_step1.RData. Delete
+
 #####Demographic#####
 abcd_pdemo <- fread(paste0(path_dat, "pdem02.txt")) %>%
   slice(-1) %>%
@@ -38,6 +41,9 @@ abcd_pdemo <- fread(paste0(path_dat, "pdem02.txt")) %>%
                 "demo_prtnr_income_v2","demo_comb_income_v2",starts_with("demo_fam_exp")) %>%
   na_if("777") %>%
   na_if("999")
+
+abcd_pdemo[,2:ncol(abcd_pdemo)] <- as.data.frame(lapply(abcd_pdemo[,2:ncol(abcd_pdemo)], as.numeric))
+
 ##remove 3 from sex (intersex-male, only 3 endorsed, out of ~10000)
 abcd_pdemo$demo_sex_v2 <- abcd_pdemo$demo_sex_v2 %>%
   na_if("3")
@@ -51,12 +57,21 @@ abcd_medServ <- fread(paste0(path_dat, "abcd_lpmh01.txt")) %>%
   dplyr::select(-c("eventname","dataset_id","collection_title")) %>%
   dplyr::select("subjectkey","medhx_1a_l") %>%
   na_if(6)
+
 abcd_medServ[,2:ncol(abcd_medServ)] <- as.data.frame(lapply(abcd_medServ[,2:ncol(abcd_medServ)], as.numeric))
 
 
 #####Teacher form####
 #https://nda.nih.gov/data_structure.html?short_name=abcd_ssbpmtf01
+abcd_pmonitor <- fread(paste0(path_dat, "abcd_ssbpmtf01.txt")) %>%
+  slice(-1) %>%
+  filter(eventname == "baseline_year_1_arm_1") %>%
+  dplyr::select(-c("eventname","dataset_id","collection_title")) %>%
+  dplyr::select(subjectkey, bpm_t_scr_attention_r
+                #,bpm_t_scr_external_r
+                )
 
+abcd_pmonitor[,2:ncol(abcd_pmonitor)] <- as.data.frame(lapply(abcd_pmonitor[,2:ncol(abcd_pmonitor)], as.numeric))
 
 #####CBCL syndrome scales####
 abcd_cbcls <- fread(paste0(path_dat, "abcd_cbcls01.txt")) %>%
@@ -70,23 +85,37 @@ abcd_cbcls <- fread(paste0(path_dat, "abcd_cbcls01.txt")) %>%
                 #cbcl_scr_dsm5_depress_t,cbcl_scr_dsm5_anxdisord_t,cbcl_scr_dsm5_somaticpr_t,cbcl_scr_dsm5_adhd_t,cbcl_scr_dsm5_opposit_t,cbcl_scr_dsm5_conduct_t,
                 #cbcl_scr_07_sct_t,cbcl_scr_07_ocd_t,cbcl_scr_07_stress_t
   )
+
 abcd_cbcls[,2:ncol(abcd_cbcls)] <- as.data.frame(lapply(abcd_cbcls[,2:ncol(abcd_cbcls)], as.numeric))
 
 
 #####Parent KSADS####
 #https://nda.nih.gov/data_structure.html?short_name=abcd_ksad501
+
+abcd_ksads <- fread(paste0(path_dat, "abcd_ksad501.txt")) %>%
+  slice(-1) %>%
+  filter(eventname == "baseline_year_1_arm_1") %>%
+  dplyr::select(-c("eventname","dataset_id","collection_title")) %>%
+  dplyr::select(subjectkey, paste0("ksads_14_",394:409,"_t"), paste0("ksads_14_",853:856,"_t")) %>%
+  na_if(555) 
+  
+abcd_ksads[,2:ncol(abcd_ksads)] <- as.data.frame(lapply(abcd_ksads[,2:ncol(abcd_ksads)], as.numeric))
+
 ##Present ADHD symptoms
-sapply(seq(394,409,by=1), function(X) paste0("ksads_14_",X,"_t")) 
+#sapply(seq(394,409,by=1), function(X) paste0("ksads_14_",X,"_t")) 
+
 ##ADHD diagnoses (present, past, partial remission, unspecified)
-sapply(seq(853,856,by=1), function(X) paste0("ksads_14_",X,"_t")) 
+#sapply(seq(853,856,by=1), function(X) paste0("ksads_14_",X,"_t")) 
 
 
 ####merge into a single data object####
-
+abcd_merged <- list(abcd_cbcls, abcd_ksads, abcd_medServ, abcd_pdemo, abcd_pmonitor, abcd_race) %>%
+  reduce(full_join, by = "subjectkey")
 
 ####check data ####
 ##check data type (chr or numeric), mean, range, NA, missing data##
-
+skim_tee(abcd_merged)
 
 ####save to RData####
-
+setwd(path_dat)
+saveRDS(abcd_merged, "abcd_merged.RData")
